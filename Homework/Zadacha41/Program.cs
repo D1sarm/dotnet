9a5500e652cfffa7309c283042d5a98172b44d97
D1zarm <featweath1337@gmail.com>
2022-10-23 10:23:22 +0300

    dz 23.10

---
+++ Homework/Zadacha41/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int M = 5;
+int[] arr = new int [M];
+int count = 0;
+for (int i = 0; i < arr.Length; i++){
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if(arr[i] > 0 ) count++;
+}
+
+System.Console.WriteLine(count);
---
+++ Homework/Zadacha43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double b1,k1,k2,b2,x,y;
+
+Console.Write("b1 = ");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1 = ");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+k2 = Convert.ToDouble(Console.ReadLine());
+
+x = (b2-b1)/(k1-k2);
+y = (k1*(b2-b1))/(k1-k2)+b1;
+
+System.Console.WriteLine($"({x};{y})");

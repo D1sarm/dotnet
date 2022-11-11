235d2bb7ad3657223357b0c5f1571d901bd01aaf
D1zarm <featweath1337@gmail.com>
2022-11-11 14:02:11 +0300

    domashka

---
+++ Homework/Zadacha50/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+//значение этого элемента или же указание, что такого элемента нет.
+
+int[,] arr = {{ 1, 4, 7, 2 },
+              { 5, 9, 2, 3 },
+              { 8, 4, 2, 4 }};
+int find = 17; // Искомое число
+int m = 0, n = 0;
+bool f = false;
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (find == arr[i, j]) { m = i; n = j; f = true; };
+        Console.Write("{0}\t", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+if (f == true)
+    Console.WriteLine($"Число найдено. Строка {m + 1} Столбец {n + 1}");
+else
+    Console.WriteLine("Такого числа в массиве нет");
---
+++ Homework/Zadacha58/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// 2 4 | 3 4
+// 3 2 | 3 3
+
+int[,] matrix1 = new int[2, 2] { { 2, 4 },
+                                 { 3, 2 } };
+int[,] matrix2 = new int[2, 2] { { 3, 4 },
+                                 { 3, 3 } };
+Console.WriteLine("Результирующая матрица будет:");
+int[,] matrix3 = new int[2, 2];
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        matrix3[i, j] = 0;
+        for (int k = 0; k < 2; k++)
+        {
+            matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+        }
+    }
+}
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.Write(matrix3[i, j] + " ");
+    }
+    Console.WriteLine();
+}
